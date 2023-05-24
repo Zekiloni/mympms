8bd359448de8405d03814073fbc90b996c93194f
Zekiloni <zekirija17@gmail.com>
2023-05-24 23:29:32 +0200

    Entity & Repository: Customer & MobilePlan

--- src/main/java/com/mympms/v1/entity/MobilePlan.java
+++ src/main/java/com/mympms/v1/entity/MobilePlan.java
@@ -1,20 +1,69 @@
 package com.mympms.v1.entity;
 
-
 import java.time.LocalDateTime;
-import java.util.Currency;
 
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name = "mobile_plans")
 public class MobilePlan {
-    public Integer id;
-    public String name;
-    public int price;
-    public Float discount;
-    public LocalDateTime createdAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private int price;
+    private Float discount;
+    private LocalDateTime createdAt;
+
+    public MobilePlan() {
+    }
 
     public MobilePlan(String name, int price) {
         this.name = name;
         this.price = price;
-
         this.createdAt = LocalDateTime.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
