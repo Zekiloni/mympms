d83c7f65639ad2f6ebb8f544694d5389d80f66b5
Zekiloni <zekirija17@gmail.com>
2023-05-24 23:41:27 +0200

    Payment Service: create payment

---
+++ src/main/java/com/mympms/v1/service/PaymentService.java
@@ -0,0 +1,33 @@
+package com.mympms.v1.service;
+
+
+import com.mympms.v1.entity.Customer;
+import com.mympms.v1.entity.MobilePlan;
+import com.mympms.v1.entity.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.mympms.v1.repository.PaymentRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Service
+public class PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Payment create(Customer customer, MobilePlan mobilePlan, BigDecimal amount) {
+        Payment payment = new Payment();
+        payment.setCustomer(customer);
+        payment.setMobilePlan(mobilePlan);
+        payment.setAmount(amount);
+        payment.setPaymentDate(LocalDateTime.now());
+
+        return paymentRepository.save(payment);
+    }
+}
