dbee13a4282e9ac91fefba5de84edf932c6dcfa5
Zekiloni <zekirija17@gmail.com>
2023-05-26 00:37:07 +0200

    CustomerService completed with interface implementing

--- src/main/java/com/mympms/v1/service/CustomerService.java
+++ src/main/java/com/mympms/v1/service/CustomerService.java
@@ -11,14 +11,36 @@ import com.mympms.v1.entity.Customer;
 
 
 @Service
-public class CustomerService {
+public class CustomerService implements CustomerServiceInterface {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Override
+    public void create(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void update(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        customerRepository.delete(customer);
+    }
+
+    @Override
+    public void deleteMany(List<Customer> customers) {
+        customerRepository.deleteAll(customers);
+    }
+
+    @Override
     public List<Customer> getAll() {
         return customerRepository.findAll();
     }
 
+    @Override
     public Optional<Customer> getOneById(int id) {
         return  customerRepository.findById(id);
     }
