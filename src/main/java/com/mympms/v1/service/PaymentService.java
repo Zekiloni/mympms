335bcc12cfb3fa06f131f4cafd8b3587cfaef63e
Zekiloni <zekirija17@gmail.com>
2023-05-27 21:50:49 +0200

    PaymentService completed

--- src/main/java/com/mympms/v1/service/PaymentService.java
+++ src/main/java/com/mympms/v1/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.mympms.v1.service;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,39 @@ import com.mympms.v1.entity.Payment;
 
 
 @Service
-public class PaymentService {
-    @Autowired
-    private  PaymentRepository paymentRepository;
+public class PaymentService implements BaseServiceInterface<Payment> {
 
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+
+    @Override
     public Payment create(Payment payment) {
         return paymentRepository.save(payment);
     }
 
+    @Override
+    public void update(Payment payment) {
+        paymentRepository.save(payment);
+    }
+
+    @Override
+    public void delete(Payment payment) {
+        paymentRepository.delete(payment);
+    }
+
+    @Override
+    public void deleteMany(List<Payment> payments) {
+        paymentRepository.deleteAll(payments);
+    }
+
+    @Override
+    public List<Payment> getAll() {
+        return paymentRepository.findAll();
+    }
+
+
+    @Override
     public Optional<Payment> getOneById(int paymentId) {
         return paymentRepository.findById(paymentId);
     }
